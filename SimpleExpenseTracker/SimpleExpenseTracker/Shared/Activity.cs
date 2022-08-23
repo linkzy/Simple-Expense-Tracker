29725ae4725dc5c81a1124c865dbb71760766fea
LinKzy <danilo.ut@gmail.com>
2022-08-23 19:56:41 -0300

    deleted 3.1 project and created .net 6 project

---
+++ SimpleExpenseTracker/SimpleExpenseTracker/Shared/Activity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleExpenseTracker.Shared
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ SimpleExpenseTracker/SimpleExpenseTracker/Shared/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleExpenseTracker.Shared
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] Salt { get; set; } = new byte[32];
+
+        public int ActiveAccountId { get; set; }
+        public Account UserAccount { get; set; }
+    }
+}
