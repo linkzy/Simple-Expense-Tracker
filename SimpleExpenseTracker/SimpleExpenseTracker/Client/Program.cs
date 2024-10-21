30d6efa93b2d67297f8713752932e05270edb88a
Danilo Cavalcante <danilo.ut@gmail.com>
2024-10-21 15:47:13 -0300

    converting client to have main method

--- SimpleExpenseTracker/SimpleExpenseTracker/Client/Program.cs
+++ SimpleExpenseTracker/SimpleExpenseTracker/Client/Program.cs
@@ -4,13 +4,19 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SimpleExpenseTracker.Client;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
-builder.Services.AddAuthorizationCore();
-builder.Services.AddBlazoredLocalStorage();
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+        builder.Services.AddAuthorizationCore();
+        builder.Services.AddBlazoredLocalStorage();
 
-await builder.Build().RunAsync();
+        await builder.Build().RunAsync();
+    }
+}
